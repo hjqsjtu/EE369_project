0e412601d6017855a9aa0ebe4b8df39a36c752ca
hjqsjtu <46159646+hjqsjtu@users.noreply.github.com>
2018-12-28 10:02:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# 2018-11-1 , Model default configurations
+#          lEFT    DOWN   RIGHT  UP  arrow key to operation
+options = [0, 1, 2, 3]
+#
+op =      [ 65,     83,     68, 87]
+#'A' 'S'  'D' 'W'  character to operation.
+
+#operation index
+actions = [0,1,2,3]
+#           ESC
+end_game =  27
+
+LR = 1e-3
+goal_steps = 2000000
+score_requirement = 1024
+initial_games = 100
---
+++ evaluate.py
@@ -0,0 +1,28 @@
+from game2048.game import Game
+from game2048.displays import Display
+from game2048.agents import ExpectiMaxAgent as TestAgent
+
+def single_run(size, score_to_win, AgentClass, **kwargs):
+    game = Game(size, score_to_win)
+    agent = AgentClass(game, display=Display(), **kwargs)
+    agent.play(verbose=True)
+    return game.score
+
+
+if __name__ == '__main__':
+    GAME_SIZE = 4
+    SCORE_TO_WIN = 2048
+    N_TESTS = 10
+    N_LOOPS = 10
+
+    '''====================
+    Use your own agent here.'''
+    from game2048.myagents import NNAgent as TestAgent
+    '''===================='''
+
+    scores = []
+    for x in range(N_TESTS):
+        score = single_run(GAME_SIZE, SCORE_TO_WIN,
+                           AgentClass=TestAgent)
+        scores.append(score)
+    print("Average scores: @%s times" % N_TESTS, sum(scores) / len(scores))
